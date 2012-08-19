6fd3b8278088d0adcfa31df875dd832256173c67
Sveinung Dalatun <sdalatun@gmail.com>
2012-08-19 14:01:57 +0200

    Creates Saltfile from a currently empty template.

--- lib/librarian/salt/cli.rb
+++ lib/librarian/salt/cli.rb
@@ -16,6 +16,12 @@ module Librarian
       include Particularity
       extend Particularity
 
+      source_root Pathname.new(__FILE__).dirname.join("templates")
+
+      def init
+        copy_file environment.specfile_name
+      end
+
       desc "version", "Displays the version."
       def version
         say "librarian-salt v#{Librarian::Salt::VERSION}"
